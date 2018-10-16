b67330a38deab0d0804c823078da8a3a5a95c8d0
Pedro Azevedo <up201306026@fe.up.pt>
2018-10-16 20:39:36 +0100

    Printing the board working

--- test.pl
+++ test.pl
@@ -1,15 +1,30 @@
 /**/
-
-x is 1.
-
+:-use_module(library(lists)).
+	
 board([
-  [e, e, b, e, e],
-  [e, e, b, w, b],
-  [e, e, b, w, e],
-  [e, w, w, e, e],
+  [e, e, e, e, e],
+  [e, e, e, e, e],
+  [e, e, e, e, e],
+  [e, e, e, e, e],
   [e, e, e, e, e]
 ]).
 
+count_elements([],0).
+count_elements([_|Tail], N) :-    
+	count_elements(Tail, X),
+	N is X+1.
+	
+print_letter(4) :-
+	write(' A ').
+print_letter(3) :-
+	write(' B ').
+print_letter(2) :-
+	write(' C ').
+print_letter(1) :-
+	write(' D ').
+print_letter(0) :-
+	write(' E ').
+	
 print_char(e) :- 
   write(' ').
 print_char(b) :- 
@@ -29,16 +44,21 @@ print_line([Char | Chars]):-
 
 % display_game(+Board, +Player)
 display_game([], _Player):-
-	write('---------------------'),
+	write('   ---------------------'),
 	write('\n').
 	
 display_game([L | Ls], _Player) :-
-  write('---------------------'),
-  write('\n'),
-  %vwrite(x),
-  print_line(L),
-  display_game(Ls, _Player).
-
+	count_elements(Ls, Y),  
+	(Y =:= 4 -> 
+		write('\n     1   2   3   4   5  \n'), 
+		write('   ---------------------')
+		; write('   ---------------------')
+	), 
+	write('\n'),    
+	print_letter(Y),
+	print_line(L),
+	display_game(Ls, _Player).
+  
 /*
 display_game([
   [e, e, b, e, e],
